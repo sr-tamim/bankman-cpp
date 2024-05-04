7a417d72e41eb7f8e0ff0010f84d464179df0acd
Limon <maksudulhasanlimon@gmail.com>
2024-05-05 00:38:17 +0600

    Derived classes for BankAccount and Implemented Savings, Checking, and Loan account classes
    Refactor the base BankAccount class to support derived classes for
    different account types. Introduced virtual functions for deposit,
    withdraw, and calculateInterest to ensure polymorphism.
    
    Added SavingsAccount with minimum balance checks, CheckingAccount with
    overdraft limits, and LoanAccount with loan-specific methods like
    makePayment and calculateMonthlyPayment.
    
    Each derived class overrides virtual functions to implement unique
    behavior. SavingsAccount uses a minimum balance, CheckingAccount
    allows overdraft, and LoanAccount calculates loan interest and
    monthly payments.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,20 @@ using namespace std;
 
 int main()
 {
+    /* 
+    Sample : -->
+
+    BankAccount* savings = new SavingsAccount("SA123", "John Doe", 200, 50);
+    BankAccount* checking = new CheckingAccount("CA123", "Jane Doe", 200, 100);
+
+    // Testing LoanAccount :-->
+    LoanAccount* loan = new LoanAccount("LA123", "Jim Beam", 10000, 0.05, 12);
+    loan->makePayment(1000);
+    loan->calculateMonthlyPayment();
+
+    */
+
+
     cout << "Hello World!" << endl;
     return 0;
 }
